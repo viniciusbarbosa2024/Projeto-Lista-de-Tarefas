9113febbd17fdb2c77f08661b9090d7836a3f768
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-03 20:51:20 -0300

    O botão de adicionar tarefa abre outra tela

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const main = document.getElementsByTagName('main')[0]
 
 function adicionarTarefa() {
-     
+    const telaAdicionarTarefa = document.getElementById('telaAdicionarTarefa')
+
+    telaAdicionarTarefa.classList.add('abrir') //Anotar sobre classList.add
+
+    main.classList.add('ocultar')
+    
 }
