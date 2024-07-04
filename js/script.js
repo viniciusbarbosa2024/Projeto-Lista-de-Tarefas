45160fe9c11fab090cd1770c1af2efb2ee04280f
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-03 21:17:20 -0300

    Consigo acessar a telaAdicionarTarefa e a main

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const main = document.getElementsByTagName('main')[0]
+const telaAdicionarTarefa = document.getElementById('telaAdicionarTarefa')
+
+function salvarTarefa() {
+    telaAdicionarTarefa.classList.remove('abrir')
+
+    main.classList.remove('ocultar')
+}
 
 function adicionarTarefa() {
-    const telaAdicionarTarefa = document.getElementById('telaAdicionarTarefa')
-
     telaAdicionarTarefa.classList.add('abrir') //Anotar sobre classList.add
 
     main.classList.add('ocultar')
+
     
 }
