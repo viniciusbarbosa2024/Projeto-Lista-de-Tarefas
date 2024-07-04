186872fc5f0ca566d89976f042a60b30a743c0cb
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-04 06:07:16 -0300

    adicionei botão para fechar TelaAdicionarTarefa

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 const main = document.getElementsByTagName('main')[0]
+const listaTarefas = document.getElementById('tarefas')
 const telaAdicionarTarefa = document.getElementById('telaAdicionarTarefa')
+const botaoFecharTelaAdicionarTarefa =document.getElementById('botaoFecharTelaAdicionarTarefa')
 const tituloTarefa = document.getElementById('tituloTarefa')
 const botaoSalvar = document.getElementById('botaoSalvar')
 
-function salvarTarefa() {
+function FecharTelaAdicionarTarefa() {
     telaAdicionarTarefa.classList.remove('abrir')
 
     main.classList.remove('ocultar')
+}
+
+function salvarTarefa() {
+    FecharTelaAdicionarTarefa()
+
+    //Criar tarefa na página inicial
+    const criarLi = document.createElement('li')
+    listaTarefas.appendChild(criarLi)
+    listaTarefas.lastElementChild.innerHTML = tituloTarefa.value
 }
 
 function adicionarTarefa() {
@@ -14,6 +25,8 @@ function adicionarTarefa() {
 
     main.classList.add('ocultar')
 
+    
+    botaoFecharTelaAdicionarTarefa.addEventListener('click',FecharTelaAdicionarTarefa)
     botaoSalvar.addEventListener('click',salvarTarefa)
     
      
