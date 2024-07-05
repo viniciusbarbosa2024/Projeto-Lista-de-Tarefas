aba75c45b180d7e075ae5bbad51dc9a2f7966ac2
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-04 21:27:13 -0300

    Validação do título da tarefa

--- js/script.js
+++ js/script.js
@@ -12,46 +12,58 @@ function FecharTelaAdicionarTarefa() {
     main.classList.remove('ocultar')
 }
 
+function AbrirTelaAdicionarTarefa() {
+    telaAdicionarTarefa.classList.add('abrir') //Anotar sobre classList.add
+
+    main.classList.add('ocultar')
+}
+
+
 function ordenarTarefa() {
     //Ordena as tarefas em relação ao horário
     const orderHorario = Number(horarioTarefa.value.charAt(0) + horarioTarefa.value.charAt(1) + horarioTarefa.value.charAt(3) + horarioTarefa.value.charAt(4))
 
     listaTarefas.lastElementChild.setAttribute('style',`order: ${orderHorario}`)
     
-    //Ver se tá tudo ok
 }
 
 function salvarTarefa() {
-    FecharTelaAdicionarTarefa()
+    if (tituloTarefa.value == "") {
+        window.alert('O título da tarefa é obrigatório')
+        tituloTarefa.focus()
+    } else {
+        FecharTelaAdicionarTarefa()
 
-    //Criar tarefa na página inicial
-        const criarLi = document.createElement('li')
-        listaTarefas.appendChild(criarLi)
+        //Criar tarefa na página inicial
+            const criarLi = document.createElement('li')
+            listaTarefas.appendChild(criarLi)
 
-        
-        for (let c = 1;c<=2;c++) {
-            const criarSpan = document.createElement('span')
+            
+            for (let c = 1;c<=2;c++) {
+                const criarSpan = document.createElement('span')
 
-            listaTarefas.lastElementChild.appendChild(criarSpan)
-            //Anotar sobre lastElementChild
-        }
+                listaTarefas.lastElementChild.appendChild(criarSpan)
+                //Anotar sobre lastElementChild
+            }
 
-        //Acessando o span[0] do li criado
-        listaTarefas.lastElementChild.children[0].innerHTML = tituloTarefa.value
+            //Acessando o span[0] do li criado
+            listaTarefas.lastElementChild.children[0].innerHTML = tituloTarefa.value
 
-        //Acessando o span[1] do li criado
-        listaTarefas.lastElementChild.children[1].innerHTML = horarioTarefa.value
+            //Acessando o span[1] do li criado
+            listaTarefas.lastElementChild.children[1].innerHTML = horarioTarefa.value
      
-        ordenarTarefa()
+        //Ordenar as tarefas adicionadas de acordo com o horário    
+            ordenarTarefa()
+    }
+    
+    
 }
 
 function adicionarTarefa() {
-    telaAdicionarTarefa.classList.add('abrir') //Anotar sobre classList.add
-
-    main.classList.add('ocultar')
-
+    AbrirTelaAdicionarTarefa()
     
     botaoFecharTelaAdicionarTarefa.addEventListener('click',FecharTelaAdicionarTarefa)
+
     botaoSalvar.addEventListener('click',salvarTarefa)
     
     
