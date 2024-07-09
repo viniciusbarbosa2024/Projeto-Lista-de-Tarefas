36aec7a77701902655ea6e6f3a9bc500585d2791
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-09 13:53:31 -0300

    Transformei variáveis em propriedades

--- js/script.js
+++ js/script.js
@@ -1,61 +1,76 @@
-const main = document.getElementsByTagName('main')[0]
-const pNaoHaTarefas = document.getElementById('NaoHaTarefas')
-const listaTarefas = document.getElementById('tarefas')
-const telaAdicionarTarefa = document.getElementById('telaAdicionarTarefa')
-const botaoFecharTelaAdicionarTarefa =document.getElementById('botaoFecharTelaAdicionarTarefa')
-const tituloTarefa = document.getElementById('tituloTarefa')
-const horarioTarefa = document.getElementById('horarioTarefa')
-const descricaoTarefa = document.getElementById('descricaoTarefa')
-const botaoSalvar = document.getElementById('botaoSalvar')
-const telaAcessoTarefa = document.getElementById('telaAcessoTarefa')
-const botaoFecharTelaAcessoTarefa = document.getElementById('botaoFecharTelaAcessoTarefa')
-const tituloTarefaAdicionada = document.getElementById('tituloTarefaAdicionada')
-const descricaoTarefaAdicionada = document.getElementById('descricaoTarefaAdicionada')
-const botaoExcluirTarefa = document.getElementById('botaoExcluirTarefa')
+const main = {
+    conteiner: document.getElementsByTagName('main')[0],
+    pNaoHaTarefas: document.getElementById('NaoHaTarefas'),
+    listaTarefas: document.getElementById('tarefas') 
+}
+
+
+const telaAdicionarTarefa = {
+    conteiner: document.getElementById('telaAdicionarTarefa'),
+    botaoFechar: document.getElementById('botaoFecharTelaAdicionarTarefa'),
+    botaoSalvar: document.getElementById('botaoSalvar')
+}
+
+
+const txtTarefa = {
+    título: document.getElementById('tituloTarefa'),
+    horário: document.getElementById('horarioTarefa'),
+    descricao: document.getElementById('descricaoTarefa')
+}
+
+const telaAcessoTarefa = {
+    conteiner: document.getElementById('telaAcessoTarefa'),
+    botaoFechar: document.getElementById('botaoFecharTelaAcessoTarefa'),
+    título: document.getElementById('tituloTarefaAdicionada'),
+    descricao: document.getElementById('descricaoTarefaAdicionada'),
+    botaoExcluirTarefa: document.getElementById('botaoExcluirTarefa')
+}
+
+
 
 const nomesDasTarefas = []
 
 
 function FecharTelaAdicionarTarefa() {
-    telaAdicionarTarefa.classList.remove('abrir')
+    telaAdicionarTarefa.conteiner.classList.remove('abrir')
 
-    main.classList.remove('ocultar')
+    main.conteiner.classList.remove('ocultar')
 }
 
 function AbrirTelaAdicionarTarefa() {
-    telaAdicionarTarefa.classList.add('abrir') //Anotar sobre classList.add
+    telaAdicionarTarefa.conteiner.classList.add('abrir') //Anotar sobre classList.add
 
-    main.classList.add('ocultar')
+    main.conteiner.classList.add('ocultar')
 }
 
 function AbrirTelaAcessoTarefa() {
-    telaAcessoTarefa.classList.add('abrir')
+    telaAcessoTarefa.conteiner.classList.add('abrir')
 
-    main.classList.add('ocultar')
+    main.conteiner.classList.add('ocultar')
 }
 
 function FecharTelaAcessoTarefa() {
-    telaAcessoTarefa.classList.remove('abrir')
+    telaAcessoTarefa.conteiner.classList.remove('abrir')
 
-    main.classList.remove('ocultar')
+    main.conteiner.classList.remove('ocultar')
 }
 
 
 function ordenarTarefa() {
     //Ordena as tarefas em relação ao horário
-    const orderHorario = Number(horarioTarefa.value.charAt(0) + horarioTarefa.value.charAt(1) + horarioTarefa.value.charAt(3) + horarioTarefa.value.charAt(4))
+    const orderHorario = Number(txtTarefa.horário.value.charAt(0) + txtTarefa.horário.value.charAt(1) + txtTarefa.horário.value.charAt(3) + txtTarefa.horário.value.charAt(4))
 
-    listaTarefas.lastElementChild.setAttribute('style',`order: ${orderHorario}`)
+    main.listaTarefas.lastElementChild.setAttribute('style',`order: ${orderHorario}`)
     
 }
 
 function salvarTarefa() {
-    if (tituloTarefa.value == "") {
+    if (txtTarefa.título.value == "") {
         window.alert('O título da tarefa é obrigatório')
-        tituloTarefa.focus()
+        txtTarefa.título.focus()
     } else {
         FecharTelaAdicionarTarefa()
-        pNaoHaTarefas.style.display = 'none'
+        main.pNaoHaTarefas.style.display = 'none'
 
         //Guardar o nome da tarefa no vetor nomeDasTarefas
         nomesDasTarefas.push(tituloTarefa.value)
@@ -63,22 +78,22 @@ function salvarTarefa() {
         //Criar tarefa na página inicial
             const criarLi = document.createElement('li')
             //li criado com evento de clique para a função de acesso a tarefa(function acessarTarefa())
-            criarLi.setAttribute('onclick',`acessarTarefa("${tituloTarefa.value}","${descricaoTarefa.value}")`)
-            listaTarefas.appendChild(criarLi)
+            criarLi.setAttribute('onclick',`acessarTarefa("${txtTarefa.título.value}","${txtTarefa.descricao.value}")`)
+            main.listaTarefas.appendChild(criarLi)
 
             
             for (let c = 1;c<=2;c++) {
                 const criarSpan = document.createElement('span')
 
-                listaTarefas.lastElementChild.appendChild(criarSpan)
+                main.listaTarefas.lastElementChild.appendChild(criarSpan)
                 //Anotar sobre lastElementChild
             }
 
             //Acessando o span[0] do li criado para colocar o título da tarefa
-            listaTarefas.lastElementChild.children[0].innerHTML = tituloTarefa.value
+            main.listaTarefas.lastElementChild.children[0].innerHTML = txtTarefa.título.value
 
             //Acessando o span[1] do li criado para colocar o horário da tarefa
-            listaTarefas.lastElementChild.children[1].innerHTML = horarioTarefa.value
+            main.listaTarefas.lastElementChild.children[1].innerHTML = txtTarefa.horário.value
      
         //Ordenar as tarefas adicionadas de acordo com o horário    
             ordenarTarefa()
@@ -90,43 +105,43 @@ function salvarTarefa() {
 function adicionarTarefa() {
     AbrirTelaAdicionarTarefa()
 
-    tituloTarefa.value = ""
-    horarioTarefa.value = ""
-    descricaoTarefa.value = ""
+    txtTarefa.título.value = ""
+    txtTarefa.horário.value = ""
+    txtTarefa.descricao.value = ""
     
-    botaoFecharTelaAdicionarTarefa.addEventListener('click',FecharTelaAdicionarTarefa)
+    telaAdicionarTarefa.botaoFechar.addEventListener('click',FecharTelaAdicionarTarefa)
 
-    botaoSalvar.addEventListener('click',salvarTarefa)
+    telaAdicionarTarefa.botaoSalvar.addEventListener('click',salvarTarefa)
     
     
      
 }
 
 function excluirTarefa(titulo) {
-    const tarefaParaExclusao = listaTarefas.children[nomesDasTarefas.indexOf(titulo)]
+    const tarefaParaExclusao = main.listaTarefas.children[nomesDasTarefas.indexOf(titulo)]
     tarefaParaExclusao.remove() //Anotar sobre remove()
     nomesDasTarefas.splice(nomesDasTarefas.indexOf(titulo),1) //Anotar sobre splice
 
     FecharTelaAcessoTarefa()
 
-    if (listaTarefas.children[0] == undefined) {
-        pNaoHaTarefas.style.display = 'block'
+    if (main.listaTarefas.children[0] == undefined) {
+        main.pNaoHaTarefas.style.display = 'block'
     }
 }
 
 function acessarTarefa(titulo,descricao) {
-    tituloTarefaAdicionada.innerHTML = titulo
+    telaAcessoTarefa.título.innerHTML = titulo
     
     if (descricao == "") {
         descricao = "Sem descrição"
     }
 
-    descricaoTarefaAdicionada.innerHTML = `<span>Descrição:</span><p>${descricao}</p>`
+    telaAcessoTarefa.descricao.innerHTML = `<span>Descrição:</span><p>${descricao}</p>`
     
     AbrirTelaAcessoTarefa()
 
-    botaoFecharTelaAcessoTarefa.addEventListener('click',FecharTelaAcessoTarefa)
-    botaoExcluirTarefa.addEventListener('click',()=>excluirTarefa(titulo)) //Anotar sobre passagem de parâmetro via addEventListener
+    telaAcessoTarefa.botaoFechar.addEventListener('click',FecharTelaAcessoTarefa)
+    telaAcessoTarefa.botaoExcluirTarefa.addEventListener('click',()=>excluirTarefa(titulo)) //Anotar sobre passagem de parâmetro via addEventListener
 
 }
 
